4530ee325bc823e9662118a1915cf87dfd85a54d
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-05-31 10:39:50 -0700

    Accept baselines

---
+++ tests/baselines/reference/constraintsAsInferenceSites.js
@@ -0,0 +1,16 @@
+//// [constraintsAsInferenceSites.ts]
+interface Ctor<T> {
+    new(): T;
+}
+declare function create<U, C extends Ctor<U>>(ctor: C): U;
+class A { a }
+let a = create(A); // should be A
+
+
+//// [constraintsAsInferenceSites.js]
+var A = (function () {
+    function A() {
+    }
+    return A;
+}());
+var a = create(A); // should be A
